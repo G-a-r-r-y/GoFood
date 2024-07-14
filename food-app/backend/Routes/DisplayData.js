c9cd5194d4b25ed38d3bb3790dd417c15d27ef40
Garvit Manral <96380278+G-a-r-r-y@users.noreply.github.com>
2024-07-14 23:06:39 +0530

    Update DisplayData.js

--- food-app/backend/Routes/DisplayData.js
+++ food-app/backend/Routes/DisplayData.js
@@ -2,15 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/foodData", (req, res) => {
-  res.send("FoodData");
-  // try {
-  //   res.send({
-  //     food_items: global.food_items,
-  //     food_category: global.food_category,
-  //   });
-  // } catch (error) {
-  //   console.log(error.message);
-  //   res.send("Server Error");
-  // }
+  try {
+    res.send({
+      food_items: global.food_items,
+      food_category: global.food_category,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.send("Server Error");
+  }
 });
 module.exports = router;
