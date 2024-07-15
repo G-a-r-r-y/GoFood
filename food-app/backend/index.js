de79dd89f528eafb3d0e6f5a6642d62c986ea237
Garvit Manral <96380278+G-a-r-r-y@users.noreply.github.com>
2024-07-15 18:41:01 +0530

    Update index.js

--- food-app/backend/index.js
+++ food-app/backend/index.js
@@ -9,7 +9,7 @@ mongoDB();
 // })
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://go-food-frontend-black.vercel.app/"); 
+  res.setHeader("Access-Control-Allow-Origin", "https://go-food-frontend-xi.vercel.app/"); 
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With, Content-Type, Accept"
